c7de9bf80252dc95c22afa3f98951a01e5c69922
Michael Maschinoff <maschinov@gmail.com>
2022-04-22 17:10:02 +0300

    Update sql replace to vars

--- db_data/sql_replace.sql
+++ db_data/sql_replace.sql
@@ -1,4 +1,7 @@
-UPDATE wp_options SET option_value = REPLACE(option_value, 'URL_FROM_WP_OPTIONS', 'http://127.0.0.1:8888') WHERE option_name = 'home' OR option_name = 'siteurl' ;
-UPDATE wp_posts SET guid = REPLACE(guid, 'URL_FROM_WP_OPTIONS','http://127.0.0.1:8888');
-UPDATE wp_posts SET post_content = REPLACE(post_content, 'URL_FROM_WP_OPTIONS', 'http://127.0.0.1:8888');
-UPDATE wp_postmeta SET meta_value = REPLACE(meta_value, 'URL_FROM_WP_OPTIONS', 'http://127.0.0.1:8888');
+SET @old_url = 'URL_FROM_WP_OPTIONS';
+SET @new_url = 'http://127.0.0.1:8888';
+
+UPDATE wp_options SET option_value = REPLACE(option_value, @old_url, @new_url) WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET guid = REPLACE(guid, @old_url, @new_url);
+UPDATE wp_posts SET post_content = REPLACE(post_content, @old_url, @new_url);
+UPDATE wp_postmeta SET meta_value = REPLACE(meta_value, @old_url, @new_url);
